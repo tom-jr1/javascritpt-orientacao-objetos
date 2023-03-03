6c22cef46b8337b420fc26ee81de6177457d7a13
tom-jr <antonio.carlos@postoaki.com.br>
2023-03-03 05:53:57 -0300

    # 003 - Atributos/propriedades private

---
+++ annotations/004-private-properties.md
@@ -0,0 +1,19 @@
+# Atributos/propriedade Privada
+
+Em nosso sistema queremos que o saldo da conta seja alterado apenas pelos métodos que definirmos de saque e
+deposito. Assim podemos aplicar as regras que desejarmos para que não seja realizados transações de formas
+indesejadas como por exemplo um deposito de -100 ou um saque acima do valor existente em extrato e etc.
+ Para que isso seja possivel precisamos adicionar os métodos na class e privar o atributo de saldo, para que 
+assim seja impossivel alterar diretamente esse atributo.
+Sintaxe de um att private:
+
+~~~ javascript
+class ClassName {
+#att1, //a cerqui esta definindo o att como privado
+#att2,
+#att3,
+}
+~~~
+
+com essa definição fica restrito a  alteração fora da proria class.
+
--- src/index.js
+++ src/index.js
@@ -5,18 +5,35 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    saldo;
+    #saldo;
 
+    constructor() {
+        this.#saldo = 0.0;
+    }
     sacar(valorSaque) {
-        const saldoValid = this.saldo > 0;
-        const valorSaqueValid = valorSaque <= this.saldo;
+        const saldoValid = this.#saldo > 0;
+        const valorSaqueValid = valorSaque <= this.#saldo;
 
         if (saldoValid && valorSaqueValid) {
-            this.saldo -= valorSaque;
+            this.#saldo -= valorSaque;
         } else {
             console.log(!saldoValid ? 'Sem saldo' : 'Saldo insuficiente');
         }
     }
+
+    depositar (valor) {
+        const valorInvalido = valor <= 0;
+        if (valorInvalido) {
+            console.log(`valor invalido ${valor}`)
+        } else {
+            this.#saldo += valor;
+        }
+    }
+
+    toString () {
+        console.log(this);
+    }
+
 }
 const cliente01 =  new Cliente();
 cliente01.nome = 'Naruto';
@@ -24,7 +41,6 @@ cliente01.cpf = '22368897411';
 
 const contaCorrente01 =  new ContaCorrente();
 contaCorrente01.agencia = '2001';
-contaCorrente01.saldo = 100.0;
 
 const cliente02 =  new Cliente();
 cliente02.nome = 'Sakura';
@@ -32,7 +48,7 @@ cliente02.cpf = '44568879986';
 
 contaCorrente02 =  new ContaCorrente();
 contaCorrente02.agencia = '2001';
-contaCorrente02.saldo = 0.0;
 
+contaCorrente01.depositar(1000)
 contaCorrente01.sacar(100);
-console.log(contaCorrente01)
+contaCorrente01.toString();
